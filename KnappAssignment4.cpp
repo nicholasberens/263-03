a0e81b43c4c1a289e6d9d5a85997620a05911d02
knappju <knappju@mail.gvsu.edu>
2020-06-15 19:52:35 -0400

    functional determineOptimalValue

--- KnappAssignment4.cpp
+++ KnappAssignment4.cpp
@@ -12,6 +12,7 @@
 *####################################################################*/
 #include <iostream>
 #include <string>
+#include <climits>
 
 using namespace std;
 
@@ -37,6 +38,7 @@ public:
 void loadRequests();
 void testFunction();
 int determineOptimalValue(int values[], int n);
+int max(int a, int b) ;
 
 /***********************************************************************
  * @Function: main
@@ -50,7 +52,7 @@ int main (){
 }
 
 /***********************************************************************
- * @Function: 
+ * @Function: loadRequests
  * @Description: 
  * @Param: n/a
  * @Returns: n/a
@@ -70,7 +72,8 @@ void testFunction(){
 	//manually entered array
 	int arr[] = {150,301,472,592,752,912,1100,1220,1368,1499};
 	int n = sizeof(arr) / sizeof(int);
-	determineOptimalValue(arr, n);
+	int q = determineOptimalValue(arr, n);
+	cout << q << endl;
 }
 
 /***********************************************************************
@@ -80,6 +83,27 @@ void testFunction(){
  * @Returns: n/a
  **********************************************************************/
 int determineOptimalValue(int values[], int n){
-	//recursive solution in here
-	return 0;
+   int val[n+1]; 
+   val[0] = 0; 
+   int i, j; 
+
+   for (i = 1; i<=n; i++) 
+   { 
+       int max_val = INT_MIN; 
+       for (j = 0; j < i; j++) 
+         max_val = max(max_val, values[j] + val[i-j-1]); 
+       val[i] = max_val; 
+   } 
+  
+   return val[n]; 
+}
+
+/***********************************************************************
+ * @Function: max
+ * @Description: 
+ * @Param: n/a
+ * @Returns: n/a
+ **********************************************************************/
+int max(int a, int b) { 
+	return (a > b)? a : b;
 }
