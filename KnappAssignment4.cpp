5d44009b10dca48d7200ccfaaf175f37bd07bb6a
knappju <knappju@mail.gvsu.edu>
2020-06-15 19:31:54 -0400

    building the project fondation

--- KnappAssignment4.cpp
+++ KnappAssignment4.cpp
@@ -11,8 +11,32 @@
 *
 *####################################################################*/
 #include <iostream>
+#include <string>
 
-using namespace std; 
+using namespace std;
+
+ 
+class request{
+	
+private:
+	string name;
+	unsigned int availableQuantity;
+	double prices[];
+	
+public:
+	request();//should never call
+	request(string name,unsigned int availableQuantity): name(name), availableQuantity(availableQuantity){}
+	
+	//setters
+	void setPrices(int values[]){
+		//this should fill this requests prices.
+	}
+	//getters
+};
+
+void loadRequests();
+void testFunction();
+int determineOptimalValue(int values[], int n);
 
 /***********************************************************************
  * @Function: main
@@ -21,7 +45,7 @@ using namespace std;
  * @Returns: int 0 confirming main function completed
  **********************************************************************/
 int main (){
-
+  testFunction();
   return 0;
 }
 
@@ -31,6 +55,31 @@ int main (){
  * @Param: n/a
  * @Returns: n/a
  **********************************************************************/
-void testFunction(){
+void loadRequests(){
+	//read in all requests from a given file. 
+	//place requests in array to be worked through?
+}
 
+/***********************************************************************
+ * @Function: 
+ * @Description: 
+ * @Param: n/a
+ * @Returns: n/a
+ **********************************************************************/
+void testFunction(){
+	//manually entered array
+	int arr[] = {150,301,472,592,752,912,1100,1220,1368,1499};
+	int n = sizeof(arr) / sizeof(int);
+	determineOptimalValue(arr, n);
+}
+
+/***********************************************************************
+ * @Function: determineOptimalValue
+ * @Description: 
+ * @Param: n/a
+ * @Returns: n/a
+ **********************************************************************/
+int determineOptimalValue(int values[], int n){
+	//recursive solution in here
+	return 0;
 }
