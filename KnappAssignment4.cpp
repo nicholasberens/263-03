a58c0a117e30135d6a081a3e4c919b2290385df6
knappju <knappju@mail.gvsu.edu>
2020-06-15 18:56:17 -0400

    Project Start

---
+++ KnappAssignment4.cpp
@@ -0,0 +1,36 @@
+/*#####################################################################
+* File:  KnappAssignment4.cpp
+*
+* Written By: Justin Knapp
+*
+* Created On: 6/15/2020
+*
+*********************************************************************
+*
+* Detail Description: The projects main driving functions
+*
+*####################################################################*/
+#include <iostream>
+
+using namespace std; 
+
+/***********************************************************************
+ * @Function: main
+ * @Description: The projects main driving functions
+ * @Param: n/a
+ * @Returns: int 0 confirming main function completed
+ **********************************************************************/
+int main (){
+
+  return 0;
+}
+
+/***********************************************************************
+ * @Function: 
+ * @Description: 
+ * @Param: n/a
+ * @Returns: n/a
+ **********************************************************************/
+void testFunction(){
+
+}
